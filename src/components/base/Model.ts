52f6e3238040ce2e4c7420b885d146b80fb425c2
goldbergz <zolotogornaya22@mail.ru>
2026-02-18 16:47:54 +0300

    feat: add base model class

---
+++ src/components/base/Model.ts
@@ -0,0 +1,32 @@
+import { ChangeListener, IModel } from '../../types/base/Model';
+
+export abstract class Model<T> implements IModel<T> {
+  protected state: T;
+  protected listeners = new Set<ChangeListener<T>>();
+
+  constructor(initialState: T) {
+    this.state = initialState;
+  }
+
+  getState(): T {
+    return this.state;
+  }
+
+  setState(state: T): void {
+    this.state = state;
+    this.emitChange();
+  }
+
+  addChangeListener(cb: ChangeListener<T>): void {
+    this.listeners.add(cb);
+  }
+
+  removeChangeListener(cb: ChangeListener<T>): void {
+    this.listeners.delete(cb);
+  }
+
+  emitChange(): void {
+    this.listeners.forEach(cb => cb(this.state));
+  }
+
+}
--- src/types/base/Model.ts
+++ src/types/base/Model.ts
@@ -1,8 +1,10 @@
 export type ChangeListener<T> = (state: T) => void;
 
 export interface IModel<T> {
+
   getState(): T;
   setState(newState: T): void;
   addChangeListener(listener: ChangeListener<T>): void;
   removeChangeListener(listener: ChangeListener<T>): void;
+  emitChange(): void;
 }
