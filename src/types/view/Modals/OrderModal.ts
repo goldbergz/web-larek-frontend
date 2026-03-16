f5e128018c084c5504f28ce82160d4e5bceb032a
goldbergz <zolotogornaya22@mail.ru>
2026-03-16 15:16:47 +0300

    docs: update readme

--- src/types/view/Modals/OrderModal.ts
+++ src/types/view/Modals/OrderModal.ts
@@ -8,7 +8,10 @@ import {
 export interface IOrderModal extends IModal {
 	setStep(step: 'payment' | 'contacts' | 'success'): void;
 	setPaymentData(data: PaymentSettings): void;
-	setContactsData(data: PaymentContacts): void;
+  setContactsData(data: PaymentContacts): void;
+  setValid(value: boolean): void;
+  onPaymentChange(callback: () => void): void;
+  onContactsChange(callback: () => void): void;
 	onNextStep?(callback: () => void): void;
 	onSubmit?(callback: (data: OrderSettings) => void): void;
 }
